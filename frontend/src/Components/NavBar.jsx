bd93b1f9dc53ca5995846acb8c0cdacd3fc46ade
Livingstone Ackah <kofiackah360@gmail.com>
2024-06-11 14:35:58 +0000

    Created a NavBar for the home page

---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { CompanyName, Logo } from "../Components/Default";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  const [open, setOpen] = useState(true);
+
+  function handleMenu() {
+    setOpen((prevOpen) => !prevOpen);
+  }
+
+  return (
+    <div className="sticky bg-red-400 -top-2 shadow-2xl block z-10">
+      {/* Nav Bar for Desktop */}
+      <nav className="flex py-2 px-5 justify-between items-center">
+        <Link to="/">
+          <div className="flex items-center">
+            <p className="md:text-4xl text-2xl">{CompanyName}</p>
+            <img
+              src={Logo}
+              alt="logo"
+              className="md:w-10 md:h-10 w-7 h-7 ml-3"
+            />
+          </div>
+        </Link>
+        <ul className="sm:flex hidden gap-5">
+          <li>
+            <Link smooth to="#home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="#about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="#project">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="#skill">
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="#contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+
+        {/* Nav Bar for Mobile */}
+        <FontAwesomeIcon
+          onClick={handleMenu}
+          icon={open ? faBars : faTimes}
+          className="sm:hidden"
+          bounce={open ? false : true}
+        />
+      </nav>
+      <ul
+        className={` ${
+          open ? "hidden" : "block"
+        } flex flex-col sm:hidden bg-secondary-200 justify-center items-center gap-5 p-5`}
+      >
+        <li>
+          <Link smooth to="#home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link smooth to="#about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link smooth to="#project">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link smooth to="#skill">
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link smooth to="#contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,18 +1,8 @@
+import NavBar from "../Components/NavBar";
 export default function Home() {
   return (
     <div>
-      <div className="relative w-32 h-32">
-        <div className="absolute w-full h-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
-        <div className="absolute w-full h-full rounded-full flex items-center justify-center">
-          <div className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-            75%
-          </div>
-        </div>
-        <div
-          className="absolute w-full h-full rounded-full bg-blue-500 flex items-center justify-center"
-          style="clip-path: inset(0 50% 0 0); transform: rotate(135deg);"
-        ></div>
-      </div>
+      <NavBar />
     </div>
   );
 }
