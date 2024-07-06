be3fad4c1f2ef39450b3eafe6171a0d91d436a35
Livingstone Ackah <kofiackah360@gmail.com>
2024-07-06 20:09:29 +0000

    Added the backend to the messages

--- frontend/src/Pages/Messages/Message.jsx
+++ frontend/src/Pages/Messages/Message.jsx
@@ -1,10 +1,53 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+// Components
 import NavBar from "../../Components/NavBar";
+import MessagesUsers from "../../Components/MessagesUsers";
+import Conversation from "../../Components/Conversation";
 
 export default function Message() {
+  const [users, setUsers] = useState([]);
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:3005/message/chatted-users",
+          {
+            withCredentials: true,
+          }
+        );
+        setUsers(res.data);
+      } catch (error) {
+        enqueueSnackbar(error.response.data.msg, { variant: "error" });
+      }
+    };
+    fetchUsers();
+  }, [enqueueSnackbar]);
+
   return (
     <div className="bg-secondary-100">
       <NavBar />
-      <p>Message Here</p>
+      <div className="md:grid md:grid-cols-3 md:m-10 lg:mx-20 xl:mx-32 2xl:mx-44 h-[33rem]">
+        <div className="overflow-auto h-full bg-white">
+          <h2 className="text-center text-xl font-bold pb-2 sticky top-0 bg-white border-b border-primary-300">
+            My Messages
+          </h2>
+          {users.map((user, index) => (
+            <Link to={`/messages/${user.username}`} key={index}>
+              <MessagesUsers username={user.username} image={user.profilePic} />
+            </Link>
+          ))}
+        </div>
+        <div className="md:col-span-2 bg-red-600">
+          <Conversation />
+        </div>
+      </div>
+      <div className="h-20"></div>
     </div>
   );
 }
