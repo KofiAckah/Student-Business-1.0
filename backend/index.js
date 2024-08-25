962e61e33c8ddc1f4d620d7c76f09ddb77de9973
Kofi Ackah <kofiackah360@gmail.com>
2024-08-25 02:57:10 +0000

    chore: Update CORS configuration to allow requests from the correct frontend URL

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    origin: "student-business-1-0.vercel.app",
+    origin: "https://student-business-1-0.vercel.app/",
     // origin: "http://localhost:5173",
     // Adjust according to your frontend URL
     credentials: true,
